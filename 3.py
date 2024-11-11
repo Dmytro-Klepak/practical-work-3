a20c0fbe6284191eb5426c23547e68681829e243
Dmytro-Klepak <dmitrooleksandrovic33@gmail.com>
2024-11-11 20:33:01 +0200

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_list = [1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
+... 
+... unique_list = remove_duplicates(my_list)
+... sorted_list = custom_sort(unique_list)
+... 
